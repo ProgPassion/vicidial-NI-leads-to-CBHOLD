41a12bd7b352a45a48677b8d0651f1abc0aba574
ProgPassion <31067252+ProgPassion@users.noreply.github.com>
2021-03-24 18:10:02 +0100

    Create justin_script.php
    Script updates all leads vicidial_list TABLE with "Not Interested " status to CBHOLD that has the modify_date column value same as the date of the "script execution day" 
    It also insert the necessary data to vicidial_callbacks TABLE to assign the callback to the specific user who set that lead as Not Interested.

---
+++ justin_script.php
@@ -0,0 +1,96 @@
+<?php
+
+	$date = date("Y-m-d");
+  
+  //assuming that the cron user credencials are like this
+	$conn = new mysqli("localhost", "cron", "1234", "asterisk");
+
+	if($conn->connect_errno) {
+		die("Failed to connect to MySQL: " . $conn->connect_error);
+	}
+
+
+	setNILeadsToCallBack($date);
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+	function queryExec($query) {
+
+		global $conn;
+
+		$result = $conn->query($query);
+		if(!$result) {
+			die($conn->error);
+		}
+
+		return $result;
+	}
+
+
+	function setDateAfterFourMonths($lead_date) {
+
+		// This function takes the lead datetime and returns it with 120 days (4 months) plus value 
+
+		$date_without_time = substr($lead_date, 0, strpos($lead_date, " "));
+		$time_from_date    = substr($lead_date, strpos($lead_date, " ") + 1);
+		$lead_date_array   = explode("-", $date_without_time);
+		
+		$lead_year  = $lead_date_array[0];
+		$lead_month = $lead_date_array[1];
+		$lead_day   = $lead_date_array[2];
+
+		return date("Y-m-d", mktime(0, 0, 0, $lead_month, $lead_day, $lead_year) + (86400 * 120)) . " " . $time_from_date;		
+	}
+
+
+	function setNILeadsToCallBack($date) {
+
+		$result = queryExec("SELECT lead_id, list_id, user, modify_date FROM vicidial_list WHERE status='NONIN' AND phone_code='39' AND modify_date LIKE'$date%'");
+		
+		while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+			$lead_id = $row['lead_id'];
+			$list_id = $row['list_id'];
+			$user    = $row['user'];
+			$modify_date = $row['modify_date'];
+
+			insertCallback($lead_id, $list_id, $modify_date, $user);
+			updateLeadStatusCBHOLD($lead_id, $user); // UPDATE status of this lead on vicidial_list TABLE
+		}
+	}
+
+	function insertCallback($lead_id, $list_id, $modify_date, $user) {
+
+		$campaign_id   = getListCampaignId($list_id);
+		$status        = "ACTIVE";
+		$entry_time    = $modify_date;
+		$callback_time = setDateAfterFourMonths($modify_date);
+		$recipient     = "USERONLY";
+		$user_group    = getUserGroup($user);
+		$lead_status   = "CALLBK";
+
+		queryExec("INSERT INTO vicidial_callbacks(lead_id, list_id, campaign_id, status, entry_time, callback_time, modify_date, user, recipient, user_group, lead_status) VALUES('$lead_id', '$list_id', '$campaign_id', '$status', '$entry_time', '$callback_time', '$modify_date', '$user', '$recipient', '$user_group', '$lead_status')");
+	}
+
+	function getUserGroup($user) {
+
+		$result = queryExec("SELECT user_group FROM vicidial_users WHERE user='$user'");
+		$user = $result->fetch_array(MYSQLI_ASSOC);
+
+		return $user["user_group"];
+	}
+
+	function getListCampaignId($list_id) {
+
+		$result = queryExec("SELECT campaign_id FROM vicidial_lists WHERE list_id='$list_id'");
+		$campaign_id = $result->fetch_array(MYSQLI_ASSOC);
+
+		return $campaign_id['campaign_id'];
+	}
+
+	function updateLeadStatusCBHOLD($lead_id, $user) {
+		queryExec("UPDATE vicidial_list SET status='CBHOLD', user='$user' WHERE lead_id='$lead_id'");
+	}
+
+?>
